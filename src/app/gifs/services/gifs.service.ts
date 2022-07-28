67b415c262dd525a2e430c1bfdcde6ecef6e6f10
Oscar Davila <oscar7822002@gmail.com>
2022-07-28 10:22:48 -0500

    Controlar el historial de búsquedas

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,8 +11,16 @@ export class GifsService {
     return [...this._historial];
   }
 
-  buscarGifs(query: string) {
-    this._historial.unshift(query);
+  buscarGifs(query: string = '') {
+
+    query = query.trim().toLocaleLowerCase();
+    
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+    }
+    
     console.log(this._historial);
+
   }
 }
